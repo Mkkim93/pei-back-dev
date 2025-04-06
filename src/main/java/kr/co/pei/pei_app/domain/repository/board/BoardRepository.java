1134b112fdcd6c09d66cd9a40b1ed34e2144fae0
mkkim <king0031443@gmail.com>
2025-04-06 19:28:07 +0900

    feat: boardRepository Entitygraph -> DTO select

--- src/main/java/kr/co/pei/pei_app/domain/repository/board/BoardRepository.java
+++ src/main/java/kr/co/pei/pei_app/domain/repository/board/BoardRepository.java
@@ -4,7 +4,6 @@ import kr.co.pei.pei_app.application.dto.board.BoardFindDTO;
 import kr.co.pei.pei_app.domain.entity.board.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -21,7 +20,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("update Board b set b.views = b.views + 1 where b.id = :boardId")
     Integer updateView(@Param("boardId") Long boardId);
 
-    @EntityGraph(attributePaths = {"users"})
     @Query("""
             select new kr.co.pei.pei_app.application.dto.board.BoardFindDTO(
             b.id, b.title, b.content, b.createdAt, b.updatedAt, b.users.name, b.users.roleType, b.views
@@ -33,7 +31,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
            """)
     Page<BoardFindDTO> searchByBoardPages(@Param("keyword") String keyword, Pageable pageable);
 
-    @EntityGraph(attributePaths = {"users"})
     @Query("""
         select new kr.co.pei.pei_app.application.dto.board.BoardFindDTO(
         b.id, b.title, b.content, b.createdAt, b.updatedAt, b.users.name, b.users.roleType, b.views) 
--- src/main/java/kr/co/pei/pei_app/domain/repository/notify/NotifyRepository.java
+++ src/main/java/kr/co/pei/pei_app/domain/repository/notify/NotifyRepository.java
@@ -1,10 +1,7 @@
 package kr.co.pei.pei_app.domain.repository.notify;
 
 import kr.co.pei.pei_app.domain.entity.notify.Notify;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
